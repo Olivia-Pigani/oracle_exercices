2ff9f4531b17a161a674f37c4c10805aecb86019
Olivia-Pigani <piganiolivia@gmail.com>
2024-12-04 14:17:39 +0100

    singleton

--- design-pattern/src/main/java/org/example/singleton/ConfigurationManager.java
+++ design-pattern/src/main/java/org/example/singleton/ConfigurationManager.java
@@ -1,31 +1,42 @@
 package org.example.singleton;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
+
 public class ConfigurationManager {
 
     private static ConfigurationManager configurationManagerInstance;
 
-    private Map<String,String> configMap = new HashMap<>();
+    private Map<String, String> configMap;
 
-    private ConfigurationManager(){
-        //fake import
-        configMap = new HashMap<>();
-        configMap.put("db.host", "localhost");
-        configMap.put("db.port", "5432");
-    };
+//    private ConfigurationManager() {
+//        //fake import
+//        configMap = new HashMap<>();
+//        configMap.put("db.host", "localhost");
+//        configMap.put("db.port", "5432");
+//    }
 
-    public static ConfigurationManager getConfigurationInstance(){
-        if(configurationManagerInstance == null){
+    private ConfigurationManager() {
+
+    }
+
+
+    public static synchronized ConfigurationManager getConfigurationInstance() {
+        if (configurationManagerInstance == null) {
             configurationManagerInstance = new ConfigurationManager();
         }
 
         return configurationManagerInstance;
     }
 
-    public String getAConfig(String key){
-        return configMap.get(key);
+    public String getAConfig(String key) {
+        return configMap.getOrDefault(key, "key do not exist");
     }
 
+    public void loadConfig(Map<String, String> configs) {
+        if (configMap == null) {
+            configMap = configs;
+        } else {
+            System.out.println("there is already an existing config map");
+        }
+    }
 }
--- design-pattern/src/main/java/org/example/singleton/Main.java
+++ design-pattern/src/main/java/org/example/singleton/Main.java
@@ -1,17 +1,32 @@
 package org.example.singleton;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Map<String,String> configMap = new HashMap<>();
+        configMap.put("db.host", "localhost");
+        configMap.put("db.port", "5432");
 
         ConfigurationManager configurationManager1 = ConfigurationManager.getConfigurationInstance();
         ConfigurationManager configurationManager2 = ConfigurationManager.getConfigurationInstance();
+        configurationManager1.loadConfig(configMap);
         System.out.println(configurationManager1.getAConfig("db.host"));
+        System.out.println(configurationManager1.getAConfig("qzzdqzdqd"));
 
         if (configurationManager1 == configurationManager2) {
             System.out.println("same object");
         }
+
+
+        Map<String,String> configMap2 = new HashMap<>();
+        configMap.put("db.host", "aaaaaa");
+        configMap.put("db.pqdqt", "54dddd32");
+
+        configurationManager1.loadConfig(configMap2);
 
     }
 }
