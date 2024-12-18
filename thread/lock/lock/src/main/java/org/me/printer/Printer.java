65a7b5ab9df414a806a2636535312df2f3e20353
Olivia-Pigani <piganiolivia@gmail.com>
2024-12-18 11:31:39 +0100

    lock

--- thread/lock/lock/src/main/java/org/me/printer/Printer.java
+++ thread/lock/lock/src/main/java/org/me/printer/Printer.java
@@ -1,27 +1,33 @@
 package org.me.printer;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Printer {
 
-    private static ReentrantLock lock = new ReentrantLock();
+    private static final Lock lock = new ReentrantLock();
 
     public Printer() {
     }
 
     public static void getResource() {
-        System.out.println(Thread.currentThread().getName() + " tente d'utiliser l'imprimante ... ");
 
         try {
-            if (lock.tryLock(5000, TimeUnit.MILLISECONDS)) {
-                System.out.println(Thread.currentThread().getName() + " a acquis le verrou et utilise l'imprimante");
-                Thread.sleep(2000);
-                System.out.println(Thread.currentThread().getName() + " a terminé d'utiliser l'imprimante et libère le verrou");
-                lock.unlock();
+            System.out.println(Thread.currentThread().getName() + " tente d'utiliser l'imprimante ... ");
+
+            if (lock.tryLock(2000, TimeUnit.MILLISECONDS)) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + " a acquis le verrou et utilise l'imprimante");
+                    Thread.sleep(2000);
+                } finally {
+                    System.out.println(Thread.currentThread().getName() + " a terminé d'utiliser l'imprimante et libère le verrou");
+                    lock.unlock();
+                }
             } else {
                 System.out.println(Thread.currentThread().getName() + " n'a pas pu accéder à l'imprimante");
             }
+
         } catch (InterruptedException e) {
             System.out.println(Thread.currentThread().getName() + " a été interrompu");
         }
