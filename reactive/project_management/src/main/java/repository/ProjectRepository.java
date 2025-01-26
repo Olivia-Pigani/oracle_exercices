fab6dc01052618cf7f94000e1182c8b9c3091ae5
Olivia-Pigani <piganiolivia@gmail.com>
2025-01-26 16:00:38 +0100

    oracle

--- reactive/project_management/src/main/java/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import entity.Project;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface ProjectRepository extends ReactiveCrudRepository<Project, Long> {
-}
